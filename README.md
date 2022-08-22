a3607bdc469e4bbcc22027a5f4f5f1a17e6bffc5
Денис <zolkovd@yandex.ru>
2022-08-22 18:06:18 +0400

    исправлен ридми

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
-# hw05_final
+# Yatube
+## Описание
+Yatube - соц.сеть для публикации дневников.
+Разработан по MVT архитектуре. Используется пагинация постов и кэширование. Регистрация реализована с верификацией данных, сменой и восстановлением пароля через почту. Написаны тесты, проверяющие работу сервиса.
+## Технологии
+- Python 3.9.7
+- Django 2.2.19
+### Установка
+Склонировать репозиторий себе:
+```
+git clone https://github.com/Lulufox/api_yamdb.git
+```
+Зайти в папку с проектом:
+```
+cd api_yamdb
+```
+Cоздать и активировать виртуальное окружение:
+```
+python3 -m venv env
+```
+```
+source env/bin/activate
+```
+Установить зависимости из файла requirements.txt:
+```
+python3 -m pip install --upgrade pip
+```
+```
+pip install -r requirements.txt
+```
+Создать в проекте файл .env, в нем указать SECRET_KEY="любой плейсхолдер".
 
-[![CI](https://github.com/yandex-praktikum/hw05_final/actions/workflows/python-app.yml/badge.svg?branch=master)](https://github.com/yandex-praktikum/hw05_final/actions/workflows/python-app.yml)
+Зайти в папку с файлом manage.py:
+```
+cd api_yamdb
+```
+Выполнить миграции:
+```
+python3 manage.py migrate
+```
+Запустить проект:
+```
+python3 manage.py runserver
+```
+Наполнить БД:
+```
+python3 manage.py load_db_from_static_data
+```
+
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9=-b^h+$k)ttp@q8&e61^-tz+t6q8&(euolo9p+2_#*+x&7)%l'
+SECRET_KEY = SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -80,10 +81,14 @@ MIDDLEWARE = [
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
-
+STATIC_URL = '/static/'
 ROOT_URLCONF = 'yatube.urls'
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), '/hw05_yatube/yatube/static/')
+
+if not DEBUG:
+    STATIC_ROOT = ''
 
 TEMPLATES = [
     {
@@ -157,5 +162,3 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
